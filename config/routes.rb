97ba2568f3512a20ac05d10a89271a94eb79df54
Thomas Wolfe <tomwolfe@gmail.com>
2019-07-27 11:45:11 +0000

    layout_helper title, application haml

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
   
   resources :users
+  match "/auth/:provider/callback" => "sessions#create", via: :post
+  match "/signout" => "sessions#destroy", :as => :signout, via: :delete
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
