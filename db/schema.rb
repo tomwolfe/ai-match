3a9ad331759f23c88a5a351aa3816049625763a7
Thomas Wolfe <tomwolfe@gmail.com>
2019-07-29 16:40:29 +0000

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190729155820) do
+ActiveRecord::Schema.define(version: 20190729164007) do
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20190729155820) do
     t.string   "location"
     t.string   "twitter"
     t.string   "email"
+    t.string   "about"
   end
 
 end
